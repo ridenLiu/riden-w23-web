5d7d2122c03220e20501e3e4ddac9e620820dbca
hy_liu_ze_yu <ridenLiu@163.com>
2023-12-21 15:55:58 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+
+import {ref} from "vue";
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+  console.log('running',count.value)
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>this is home</h1>
+    <button @click="increment">{{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
